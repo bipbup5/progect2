0115c5bcc93b70f70990c1e8f031fcbc754d7054
bipbup <mkopach05@gmail.com>
2021-01-16 12:40:17 +0300

    приведения

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import copy
 import os
-
+from random import choice
 
 square = 30
 top = 10
@@ -15,36 +15,33 @@ gameBoard = []
 clock = pygame.time.Clock()
 
 lvls = [[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-        [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
-        [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
-        [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
-        [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-        [1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 2, 1, 1],
-        [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1],
-        [1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 1],
-        [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1],
-        [1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
-        [1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1],
-        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]]
-
+         [1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+         [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
+         [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
+         [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
+         [1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+         [1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 2, 1, 1],
+         [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1],
+         [1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 1],
+         [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1],
+         [1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+         [1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+         [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]]
 pygame.init()
 size = width, height = 620, 620
 screen = pygame.display.set_mode(size)
-
-
 wall_sprites = pygame.sprite.Group()
 fl_sprites = pygame.sprite.Group()
 pac_sprites = pygame.sprite.Group()
 dot_sprites = pygame.sprite.Group()
-
+g_sprites = pygame.sprite.Group()
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -90,10 +87,11 @@ class Pacman:
             if gameBoard[int(self.row)][int(self.col)] == 2:
                 gameBoard[int(self.row)][int(self.col)] = 0
                 for dot in dot_sprites:
-                    if dot.rect.x == self.row * square + left + 5 and\
+                    if dot.rect.x == self.row * square + left + 5 and \
                             dot.rect.y == self.col * square + top + 5:
                         dot_sprites.remove(dot)
                         res += 1
+                        print(res)
         if dir == 3:
             if int(self.row) == self.row:
                 if canMove(self.row - 1, self.col) and self.col % 1 == 0:
@@ -124,6 +122,97 @@ class Pacman:
                     self.col -= 0.25
             else:
                 self.col -= 0.25
+
+class Ghosts:
+    def __init__(self, row, col):
+        im = load_image('g_red.png')
+        im = pygame.transform.scale(im, (20, 20))
+        self.row = row
+        self.col = col
+        self.g_sprite = pygame.sprite.Sprite()
+        g_sprites.add(self.g_sprite)
+        self.dir_g = choice((0, 1, 2, 3))
+        self.g_sprite.image = im
+        self.g_sprite.rect = self.g_sprite.image.get_rect()
+        self.draw()
+
+    def draw(self):
+        self.g_sprite.rect.x = self.row * square + square // 2 + left - 10
+        self.g_sprite.rect.y = self.col * square + square // 2 + top - 10
+        g_sprites.draw(screen)
+
+    def update(self):
+        if self.dir_g == 3:
+            if int(self.row) == self.row:
+                if canMove(self.row - 1, self.col) and self.col % 1 == 0:
+                    self.row -= 0.25
+                    self.if_turn()
+                    return
+                else:
+                    self.dir_g = choice((0, 2, 3))
+                    return
+            else:
+                self.row -= 0.25
+                self.if_turn()
+                return
+        elif self.dir_g == 2:
+            if int(self.col) == self.col:
+                if canMove(self.row, self.col + 1) and self.row % 1.0 == 0:
+                    self.col += 0.25
+                    self.if_turn()
+                    return
+                else:
+                    self.dir_g = choice((1, 2, 3))
+                    return
+            else:
+                self.col += 0.25
+                self.if_turn()
+                return
+        elif self.dir_g == 1:
+            if int(self.row) == self.row:
+                if canMove(self.row + 1, self.col) and self.col % 1.0 == 0:
+                    self.row += 0.25
+                    self.if_turn()
+                    return
+                else:
+                    self.dir_g = choice((0, 1, 2))
+                    return
+            else:
+                self.row += 0.25
+                self.if_turn()
+                return
+        elif self.dir_g == 0:
+            if int(self.col) == self.col:
+                if canMove(self.row, self.col - 1) and self.row % 1.0 == 0:
+                    self.col -= 0.25
+                    self.if_turn()
+                else:
+                    self.dir_g = choice((0, 1, 3))
+                    return
+            else:
+                self.col -= 0.25
+                self.if_turn()
+                return
+
+    def if_turn(self):
+        if self.col % 1 == 0 and self.row % 1 == 0:
+            if self.dir_g == 3:
+                if gameBoard[int(self.row)][int(self.col) + 1] in [0, 2] or gameBoard[int(self.row)][int(self.col) - 1] in [0, 2]:
+                    self.dir_g = choice((0, 2, 3))
+
+            elif self.dir_g == 2:
+                if gameBoard[int(self.row) + 1][int(self.col)] in [0, 2] or gameBoard[int(self.row) - 1][int(self.col)] in [0, 2]:
+                    self.dir_g = choice((1, 2, 3))
+
+            elif self.dir_g == 1:
+                if gameBoard[int(self.row)][int(self.col) + 1] in [0, 2] or gameBoard[int(self.row)][int(self.col) - 1] in [0, 2]:
+                    self.dir_g = choice((0, 1, 2))
+
+            elif self.dir_g == 0:
+                if gameBoard[int(self.row) + 1][int(self.col)] in [0, 2] or gameBoard[int(self.row) - 1][int(self.col)] in [0, 2]:
+                    self.dir_g = choice((0, 1, 3))
+
+
 
 
 class Game:
@@ -168,7 +257,6 @@ class Game:
                     fl.rect.x = x * self.cell_size + self.left
                     fl.rect.y = y * self.cell_size + self.top
                     fl_sprites.add(fl)
-
                     dot = pygame.sprite.Sprite()
                     dot.image = im_dot
                     dot.rect = dot.image.get_rect()
@@ -179,6 +267,11 @@ class Game:
 
 game = Game(1)
 pacman = Pacman(1, 1)
+ghost = Ghosts(5, 1)
+ghost2 = Ghosts(6, 1)
+ghost3 = Ghosts(7, 1)
+ghost4 = Ghosts(8, 1)
+
 running = True
 game.render()
 while running:
@@ -199,9 +292,16 @@ while running:
     wall_sprites.draw(screen)
     dot_sprites.draw(screen)
     pacman.draw()
+    ghost.draw()
+    ghost2.draw()
+    ghost3.draw()
+    ghost4.draw()
     k += v * clock.tick() / 1000
     if k >= 30:
         pacman.update()
+        ghost.update()
+        ghost2.update()
+        ghost3.update()
+        ghost4.update()
         k = 0
     pygame.display.flip()
-    print(res)
